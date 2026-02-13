7f27bdbf6f50cb9a839ac1e292e9828ad0fd1fe5
Thaynacarvalho98 <marques.thay98@outlook.com>
2026-02-13 11:44:07 -0300

    criando sistma de cadastro de usuario

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-def saudacao(nome):
-    return f"Olá, {nome}!\nBem-vindo ao meu projeto em python."
-def menu():
-    nome = input("Digite seu nome: ")
-    print(saudacao(nome))
-    if __name__ == "__main__":
-        menu()
+usuarios = []
+def adicionar_usuario():
+    nome = input("Nome do usuário: ")
+    idade = input("idade: ")
+    usuarios.append({"nome": nome, "idade": idade})
+    print("Usuário cadastrado com sucesso!\n")
+
+    def listar_usuarios():
+        if not usuarios:
+            print("Nenhum usuário cadastrado.\n")
+            return
+        for i, usuario in enumerate(usuarios, start=1):
+            print( f"{i} - nome: {usuario['nome']} - idade: {usuario['idade']}")
+            print()
+            def menu():
+                while True:
+                    print("1 - Adicionar usuário")
+                    print("2 - Listar usuários")
+                    print("3 - Sair")
+                    opcao = input("Escolha uma opção: ")
+                    if opcao == "1":
+                        adicionar_usuario()
+                    elif opcao == "2":
+                        listar_usuarios()
+                    elif opcao == "3":
+                        print("Saindo...")
+                        break
+                    else:
+                        print("Opção invalida!\n")
+        if __name__ == "__main__":
+            menu()
